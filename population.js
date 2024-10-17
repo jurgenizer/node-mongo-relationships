3e7dfbc2f961a0de760ad3b0ebb0302301f27314
Jurgen Geitner <jurgen.geitner@gmail.com>
2024-10-17 18:46:00 +0200

    referencing an object

--- population.js
+++ population.js
@@ -12,6 +12,10 @@ const Author = mongoose.model('Author', new mongoose.Schema({
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author'
+  }
 }));
 
 async function createAuthor(name, bio, website) { 
@@ -42,8 +46,8 @@ async function listCourses() {
   console.log(courses);
 }
 
-createAuthor('Mosh', 'My bio', 'My Website');
+// createAuthor('Mosh', 'My bio', 'My Website');
 
-// createCourse('Node Course', 'authorId')
+createCourse('Node Course', '67113cd534305225e1f1bac3')
 
 // listCourses();
