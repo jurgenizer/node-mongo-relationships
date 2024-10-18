04dfb01ce4e7184a6f257e66fd868d47913fd3c7
Jurgen Geitner <jurgen.geitner@gmail.com>
2024-10-18 12:00:54 +0200

    embedding documents

---
+++ embedding.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+const Course = mongoose.model('Course', new mongoose.Schema({
+  name: String,
+  author: {
+    type: authorSchema,
+    required: true
+  }
+}));
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name, 
+    author
+  }); 
+  
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() { 
+  const courses = await Course.find();
+  console.log(courses);
+}
+
+/* Version 1 
+async function updateAuthor(courseId) {
+  const course = await Course.findById(courseId);
+  course.author.name = 'Jurgen Geitner';
+  course.save();
+}
+  */
+
+/* Version 2 */
+async function updateAuthor(courseId) {
+  const course = await Course.updateOne({ _id: courseId}, {
+    $set: {
+      'author.name': 'Roberto Marley'
+    }
+    /* to remove
+      $unset: {
+      'author.name': ''
+    } */
+  });
+}
+
+//createCourse('Node Course', new Author({ name: 'Mosh' }));
+
+updateAuthor('67122ac471e653e4af8c5661');
+
